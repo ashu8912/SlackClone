a723de7f4b3016afc7ee50e5f7c3d97dc9e3e35e
ashu8912 <ashughildiyal5@gmail.com>
2019-03-18 07:45:26 +0530

    user and channel reducer added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,44 @@
-import {combineReducer} from "redux";
+import {combineReducers} from "redux";
 import * as actionTypes from "../actions/types";
 
 const initialUserState={
     currentUser:null,
-    isLoading:false
+    isLoading:true
 }
-export const user_reducer=(state=initialUserState,action)=>{
+const user_reducer=(state=initialUserState,action)=>{
 switch(action.type){
     case actionTypes.SET_USER:
     return {
         currentUser:action.payload.currentUser,
-        isLoading:true
+        isLoading:false
+    }
+    case actionTypes.CLEAR_USER:
+    return {
+        currentUser:null,
+        isLoading:false
     }
     default:
     return state;
 }
 }
-const rootReducer=combineReducer({
-    user:user_reducer
+const channelInitialState={
+    currentChannel:null
+}
+const channel_reducer=(state=channelInitialState,action)=>
+{
+switch(action.type)
+{
+    case actionTypes.SET_CURRENT_CHANNEL:
+    return {
+          ...state, 
+          currentChannel:action.payload.channel
+    }
+    default:
+    return state;
+}
+}
+const rootReducer=combineReducers({
+    user:user_reducer,
+    channel:channel_reducer
 })
 export default rootReducer;
